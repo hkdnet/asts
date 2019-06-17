0c7f3f814e1c890088ac8d5a2477fb049ecf367d
hkdnet <satoko.itse@gmail.com>
2019-06-17 21:37:34 +0900

    Use passed config object

--- lib/asts.rb
+++ lib/asts.rb
@@ -16,9 +16,9 @@ module Asts
       yield config
     end
 
-    def extract
-      detector = Asts::Detector.new(config)
-      parser = Asts::Parser.new(config)
+    def extract(cfg = config)
+      detector = Asts::Detector.new(cfg)
+      parser = Asts::Parser.new(cfg)
       detector.target_files.each_with_object({}) do |f, h|
         f.asts = parser.parse(f)
         h[f.relative_path] = f
